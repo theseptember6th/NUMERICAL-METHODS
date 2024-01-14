ca3d270313a6fa055d9ba95659a77225d3e50455
theseptember6th <shresthakristal42@gmail.com>
2024-01-14 06:11:24 +0545

    inverse using gauss jordan

--- solutions of linear eqns/gauss_jordan_method/inverse_using_gauss_jordan.cpp
+++ solutions of linear eqns/gauss_jordan_method/inverse_using_gauss_jordan.cpp
@@ -94,59 +94,6 @@ void INVERSE(float **matrix, int row)
         }
     }
 }
-// Function to calculate the determinant of a square matrix
-float calculateDeterminant(float **matrix, int row)
-{
-    if (row == 1)
-    {
-        return matrix[0][0];
-    }
-    else if (row == 2)
-    {
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-    }
-    else
-    {
-        float sign = 1.0;
-        float cumulativeSum = 0;
-
-        for (int colNum = 0; colNum < row; colNum++)
-        {
-            // Create a submatrix excluding the first row and the current column
-            float **submatrix = new float *[row - 1];
-            for (int i = 0; i < row - 1; i++)
-            {
-                submatrix[i] = new float[row - 1];
-            }
-
-            for (int i = 1; i < row; i++)
-            {
-                int subIndex = 0;
-                for (int j = 0; j < row; j++)
-                {
-                    if (j != colNum)
-                    {
-                        submatrix[i - 1][subIndex++] = matrix[i][j];
-                    }
-                }
-            }
-
-            float determinant = calculateDeterminant(submatrix, row - 1);
-            cumulativeSum += matrix[0][colNum] * determinant * sign;
-
-            // Deallocate memory for the submatrix
-            for (int i = 0; i < row - 1; i++)
-            {
-                delete[] submatrix[i];
-            }
-            delete[] submatrix;
-
-            sign = -sign;
-        }
-
-        return cumulativeSum;
-    }
-}
 
 
 int main()
@@ -161,19 +108,11 @@ int main()
     {
         matrix[i] = new float[column];
     }
+    
     input(matrix,row);
-    float determinant = calculateDeterminant(matrix, row);
-    cout << "Determinant of the matrix is: " << determinant << "\n";
-
-    if (determinant == 0)
-    {
-        cout << "Error: Determinant is zero. Unable to find the inverse.\n";
-    }
-    else
-    {
-        INVERSE(matrix, row);
-        display_inverse(matrix, row);
-    }
+    INVERSE(matrix, row);
+    display_inverse(matrix, row);
+    
     cout << "\ndeleting the matrix\n\n";
 
     for (int i = 0; i < row; i++)
